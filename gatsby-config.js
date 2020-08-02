67e21aaa98bb3f46237b7a4eb876ff84ba030d5e
dikili <dikili@outlook.com>
2020-08-02 22:57:35 +0100

    gatsby init

---
+++ gatsby-config.js
@@ -0,0 +1,69 @@
+module.exports = {
+  siteMetadata: {
+    title: `A technical perspective`,
+    description: `I am passionate technologist working as a SDET manager, In this blog I intend to share my insights into some of the technical topics as well as opinions/ideas on variety of topics of interest to me`,
+    author: `Ismail Dikili`,
+    siteUrl: `https://github.com/dikili`,
+    social: {
+      twitter: `dikili11`,
+      facebook: ``,
+      github: `dikili`,
+      linkedin: `dikili`,
+      email: `dikili@outlook.com`,
+    },
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 970,
+            },
+          },
+          `gatsby-remark-prismjs`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        //trackingId: `ADD YOUR TRACKING ID HERE`,
+      },
+    },
+    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `flexible-gatsby-starter`,
+        short_name: `flexible-gatsby`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `./static/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+  ],
+}
